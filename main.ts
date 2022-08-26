8b2e84fa739d8722496e0d23d6915e4a30486fd0
kewcow <kevjhoss@gmail.com>
2022-08-26 17:46:13 -0300

    version 0.2

--- main.ts
+++ main.ts
@@ -1,11 +1,34 @@
 import {serve} from './deps.ts';
 import {home} from './router.ts';
 
+const headers: any = (arch: string): object => {
+  let type: {headers: any} = {
+    headers:<object> {
+      'Content-Type':<string> 'text/html',
+      'Access-Control-Allow-Origin':<string> '*',
+      'Access-Control-Allow-Headers':<string> '*',
+    }
+  }
+
+  if (arch === 'css') type.headers['Content-Type']= 'text/css';
+  if (arch === 'js') type.headers['Content-Type']= 'application/javascript';
+  return type;
+};
+
 const handler = async (req: Request): Promise<Response> => {
   const pathname = new URL(req.url).pathname;
+  const pattern: URLPattern = new URLPattern({pathname: '/food/:id', });
+  const patternAdmin: URLPattern = new URLPattern({pathname: '/user/:id', });
   if (pathname === '/') return home();
-  const file = await Deno.readFile(`${Deno.cwd()}/dist${pathname}`);
-  return new Response(file);
+  if (pathname === '/login') return home();
+  if (pattern.test(req.url)) return home();
+  if (patternAdmin.test(req.url)) return home();
+
+  const css = await Deno.readFile(`${Deno.cwd()}/dist${pathname}`);
+  if (pathname.split('.').at(-1) === 'css') return new Response(css, headers('css'));
+
+  const js = await Deno.readFile(`${Deno.cwd()}/dist${pathname}`);
+  return new Response(js, headers('js'));
 }
 
 const env = Deno.env.get('PORT');
--- router.ts
+++ router.ts
@@ -1,6 +1,21 @@
+const headers: any = (arch: string): object => {
+  let type: {headers: any} = {
+    headers:<object> {
+      'Content-Type':<string> 'text/html',
+      'Access-Control-Allow-Origin':<string> '*',
+      'Access-Control-Allow-Headers':<string> '*',
+    }
+  }
+
+  if (arch === 'json') type.headers['Content-Type']= 'application/json';
+  if (arch === 'html') type.headers['Content-Type']= 'text/html';
+
+  return type;
+};
+
 const home = async (): Promise<Response> => {
   const file = await Deno.readFile(`./dist/index.html`);
-  return new Response(file)
+  return new Response(file, headers())
 }
 
 export {
